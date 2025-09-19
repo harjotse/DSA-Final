d5a1618a146b51f66af920cc7f9b3343a8bfb5b0
Harjot Singh <harjotsingh@Harjots-MacBook-Pro.local>
2025-09-19 09:41:05 +0530

    stack q3

---
+++ stacks and queues/NGR.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        Stack<Integer>st=new Stack<>();
+        ArrayList<Integer>ans=new ArrayList<>();
+        int nums []= new int[arr.length];
+         st.push(arr[arr.length-1]);
+         nums[arr.length-1]=-1;
+         for(int i=arr.length-2;i>=0;i--){
+            if(st.peek()>arr[i]){
+              nums[i]=st.peek();
+            }
+            else{
+                while(!st.isEmpty() && st.peek()<=arr[i]){
+                    st.pop();
+                }
+                if(!st.isEmpty())
+                    nums[i]=st.peek();
+                else
+                   nums[i]=-1;
+            }
+            st.push(arr[i]);
+         }
+         for(int element : nums){
+             ans.add(element);
+         }
+      return ans;  
+    }
+}
